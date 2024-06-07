e4d87bd94d89aab41de2496dce055895d74f7a79
Keyur Bhimani <keyurbhimani2000@gmail.com>
2024-06-07 11:06:44 +0530

    done

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
         let classes = "flex flex-col gap-4 border-2 border-gray-400 hover:text-purple-500 hover:border-purple-500  py-2 px-5 rounded-lg cursor-pointer  transition-all duration-500 "
         if (type === subpages) {
             if(subpages === "ecommerce"){
-                return classes += "bg-gradient-to-r from-gray-700 py-2.3 via-gray-500 w-[115%]   to-gray-300 overflow-hidden hover:text-white text-white  border-gray-500 hover:border-sky-500"
+                return classes += "bg-gradient-to-r from-gray-700 py-2.3 via-gray-500 w-[115%]   to-gray-300 overflow-hidden hover:text-white text-white  border-gray-500 hover:border-gray-500"
             }
             if(subpages === "blogapp"){
                 return classes += "bg-gradient-to-r from-blue-600 py-2.3 via-sky-500 w-[115%]   to-sky-300 overflow-hidden hover:text-white text-white  border-sky-500 hover:border-sky-500"
