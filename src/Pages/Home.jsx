82732f50b71ff58b3bf5a0cc9accb22d3a56d67c
Keyur Bhimani <keyurbhimani2000@gmail.com>
2024-06-19 11:26:12 +0530

    changed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ import Footer from '../Components/Footer'
 
 const Home = () => {
   return (
-    <div className='w-[80%] md:w-[100%] flex justify-center flex-col  '>
+    <div className='w-[80%] md:w-[100%] flex justify-center flex-col py-10 '>
         <Sidebar />
     </div>
   )
